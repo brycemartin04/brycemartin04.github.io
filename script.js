2484bc82f6173540e244a08b020972a18860d5d7
brycemartin04 <brycemartin@ku.edu>
2025-01-20 17:25:39 -0600

    rework animation

--- script.js
+++ script.js
@@ -1,17 +1,14 @@
 
 // JavaScript to handle scroll or click animations
 const intro = document.getElementById('intro');
+const introContent = document.getElementById('intro-content');
 const mainContent = document.getElementById('main-content');
-const greeting = document.getElementById('greeting');
-const greeting2 = document.getElementById('greeting2');
 const name = document.getElementById('name')
 
 function transitionContent() {
     // Add the "scrolled" class to the intro
     intro.classList.add('shrink');
-    greeting.classList.add('fade-out');
-    greeting2.classList.add('fade-out');
-    
+    introContent.classList.add('shrink');
     // Show main content
     mainContent.style.transform = 'translateY(-100vh)';
 }
